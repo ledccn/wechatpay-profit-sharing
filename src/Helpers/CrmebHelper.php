eb5247a3999a7fb6a485009211ddad6a2a0a7ae7
david <367013672@qq.com>
2024-10-17 11:38:06 +0800

    完成：crmeb单商户分账

--- src/Helpers/CrmebHelper.php
+++ src/Helpers/CrmebHelper.php
@@ -2,18 +2,28 @@
 
 namespace Ledc\WechatPayProfitSharing\Helpers;
 
+use app\model\order\StoreOrder;
+use app\model\user\User;
+use app\model\user\UserExtract;
+use app\services\user\UserBrokerageServices;
 use app\services\user\UserServices;
 use app\services\wechat\WechatUserServices;
+use ErrorException;
+use InvalidArgumentException;
 use Ledc\WechatPayProfitSharing\Contracts\Receiver;
 use Ledc\WechatPayProfitSharing\Contracts\ReceiverTypeEnums;
+use Ledc\WechatPayProfitSharing\Exceptions\AutoProfitSharingException;
 use Ledc\WechatPayProfitSharing\Exceptions\HttpException;
-use Ledc\WechatPayProfitSharing\Helper;
 use Ledc\WechatPayProfitSharing\Utils;
 use think\db\exception\DataNotFoundException;
+use think\facade\Db;
+use think\facade\Log;
+use Throwable;
 
 /**
  * CRMEB单商户
  * - 版本号：CRMEB-BZ v5.4.0(20240708)
+ * - 使用步骤：订单表eb_store_order新增wechat_pay_profit_sharing_finish字段（默认值1，避免处理以往订单）；然后，把默认值改为0；
  */
 class CrmebHelper
 {
@@ -40,6 +50,229 @@ class CrmebHelper
     }
 
     /**
+     * 计划任务调度：自动分账
+     * @return void
+     */
+    public static function scheduler(): void
+    {
+        $three_days_ago = 86400 * 2;
+        $query = StoreOrder::where('paid', '=',1)
+            ->where('pay_time', '>=', time() - $three_days_ago)
+            ->where('refund_status', '=', 0)
+            ->where('pay_type', '=', 'weixin');
+        $query->chunk(100, function ($orders) {
+            /** @var StoreOrder $order */
+            foreach ($orders as $order) {
+                try {
+                    CrmebHelper::autoProfitSharing($order->id);
+                    $order->mark = '微信自动分账，已完成';
+                    $order->save();
+                } catch (Throwable $e) {
+                    $order->mark = $e->getMessage();
+                    $order->save();
+                }
+            }
+        });
+    }
+
+    /**
+     * 自动分账
+     * @param int $id 订单表主键
+     * @return void
+     * @throws ErrorException
+     */
+    public static function autoProfitSharing(int $id): void
+    {
+        try {
+            $storeOrder = static::verifyOrderBy($id);
+
+            $list = static::getExecuteList($storeOrder);
+            if (empty($list)) {
+                // 完结分账（请求需要双向证书）
+                Helper::finish($storeOrder->trade_no, $storeOrder->order_id, '该订单无需分账（无分账接收方），已完成');
+            } else {
+                // 查询订单待分账金额
+                $_rs = Helper::orderAmountQuery($storeOrder->trade_no);
+                var_dump('查询订单待分账金额【响应】', $_rs);
+
+                $site_name = sys_config('site_name');
+                $user_phone = substr_replace($storeOrder->user_phone, '****', 3, 4);
+
+                /**
+                 * 数据库事务
+                 * @description 以下逻辑来自 \app\services\user\UserExtractServices::cash
+                 */
+                Db::transaction(function () use ($list, $storeOrder, $_rs, $site_name, $user_phone) {
+                    $trade_no = $storeOrder->trade_no;
+                    $order_id = $storeOrder->order_id;
+
+                    $receivers = [];
+                    foreach ($list as $item) {
+                        [$uid, $brokerage] = $item;
+                        $profitUser = static::verifyBrokerageByUser($uid, $brokerage);
+                        if (!$profitUser) {
+                            continue;
+                        }
+
+                        $mark = "用户{$user_phone}在{$site_name}消费，订单{$order_id}分账佣金";
+                        $extract_price = bcmul($brokerage, '1', 2);
+
+                        // 插入：用户提现表
+                        $insertUserExtract = [
+                            'uid' => $uid,
+                            'real_name' => $profitUser->real_name ?: $profitUser->nickname,
+                            'extract_type' => 'weixin',
+                            'extract_price' => $extract_price,
+                            'extract_fee' => 0,
+                            'add_time' => time(),
+                            'balance' => $profitUser->brokerage_price,
+                            'status' => 1, // -1 未通过 0 审核中 1 已提现
+                        ];
+                        $userExtractModel = UserExtract::create($insertUserExtract);
+
+                        // 更新：用户表的用户余额
+                        $balance = max(bcsub((string)$profitUser->brokerage_price, $extract_price, 2), 0);
+                        $profitUser->brokerage_price = $balance;
+                        $profitUser->save();
+
+                        // 插入：用户分佣账单表
+                        /** @var UserBrokerageServices $userBrokerageServices */
+                        $userBrokerageServices = app()->make(UserBrokerageServices::class);
+                        $userBrokerageServices->income('extract', $uid, ['mark' => $mark, 'number' => $extract_price], $balance, $userExtractModel->id);
+
+                        $openid = CrmebHelper::getUserOpenid($uid);
+
+                        // 添加分账接收方
+                        $response = Helper::api()->addReceiver(Utils::packReceiver($openid));
+                        $response->toArray();
+                        //var_dump($_result);
+
+                        $receivers[] = new Receiver([
+                            'type' => ReceiverTypeEnums::PERSONAL_OPENID,
+                            'account' => $openid,
+                            'amount' => (int)bcmul($brokerage, '100'),
+                            'description' => $mark
+                        ]);
+                    }
+
+                    if (empty($receivers)) {
+                        // 完结分账（请求需要双向证书）
+                        Helper::finish($trade_no, $order_id, '该订单无需分账（手动提现），已完成');
+                    } else {
+                        $result = Helper::singleSharing($receivers, $trade_no, $storeOrder->order_id);
+                        var_dump($result);
+                        $storeOrder->wechat_pay_profit_sharing_finish = 1;
+                        $storeOrder->save();
+                    }
+                });
+            }
+        } catch (Throwable $throwable) {
+            Log::error($throwable->getMessage());
+            throw new ErrorException($throwable->getMessage(), $throwable->getCode());
+        }
+    }
+
+    /**
+     * 验证订单
+     * @param int $id 订单表主键
+     * @return StoreOrder
+     * @throws AutoProfitSharingException
+     */
+    protected static function verifyOrderBy(int $id): StoreOrder
+    {
+        /** @var StoreOrder $storeOrder */
+        $storeOrder = StoreOrder::findOrEmpty($id);
+        if ($storeOrder->isEmpty()) {
+            throw new InvalidArgumentException('订单不存在');
+        }
+        if ($storeOrder->wechat_pay_profit_sharing_finish) {
+            throw new InvalidArgumentException('当前订单已完结分账');
+        }
+
+        if ($storeOrder->refund_status) {
+            throw new InvalidArgumentException('当前订单正在申请退款或已退款，不支持分账');
+        }
+
+        $trade_no = $storeOrder->trade_no;
+        if (empty($trade_no)) {
+            throw new InvalidArgumentException('微信支付订单号为空，无法分账');
+        }
+
+        if (empty($storeOrder->paid && $storeOrder->pay_time)) {
+            throw new InvalidArgumentException('订单未支付，无法分账');
+        }
+
+        // 检查开关：0=>线下手动转账；1=>自动到微信零钱
+        if (sys_config('brokerage_type', 0)) {
+            $storeOrder->wechat_pay_profit_sharing_finish = 1;
+            $storeOrder->save();
+
+            throw new AutoProfitSharingException('当前已设置到账方式为自动到微信零钱，为了保障资金安全，将跳过自动分账逻辑。');
+        }
+
+        return $storeOrder;
+    }
+
+    /**
+     * 获取待分账的执行列表
+     * @param StoreOrder $storeOrder
+     * @return array
+     */
+    protected static function getExecuteList(StoreOrder $storeOrder): array
+    {
+        $list = [];
+        if ($storeOrder->spread_uid && $storeOrder->one_brokerage && 1 === bccomp($storeOrder->one_brokerage, '0', 2)) {
+            $list[] = [$storeOrder->spread_uid, $storeOrder->one_brokerage];
+        }
+
+        if ($storeOrder->spread_two_uid && $storeOrder->two_brokerage && 1 === bccomp($storeOrder->two_brokerage, '0', 2)) {
+            $list[] = [$storeOrder->spread_two_uid, $storeOrder->two_brokerage];
+        }
+
+        return $list;
+    }
+
+    /**
+     * 验证用户可提现佣金
+     * @param int $uid
+     * @param string $brokerage 当前待分账的金额
+     * @return User|null
+     */
+    protected static function verifyBrokerageByUser(int $uid, string $brokerage): ?User
+    {
+        try {
+            /** @var User $user */
+            $user = User::findOrEmpty($uid);
+            if ($user->isEmpty()) {
+                throw new AutoProfitSharingException('用户未找到');
+            }
+
+            /** @var UserBrokerageServices $services */
+            $services = app()->make(UserBrokerageServices::class);
+
+            // 获取某个账户下的冻结佣金
+            $broken_commission = max($services->getUserFrozenPrice($uid), 0);
+            // 用户佣金总额
+            $brokerage_price = $user->brokerage_price;
+            if ($brokerage_price <= 0) {
+                throw new AutoProfitSharingException('用户的用户佣金总额为空。');
+            }
+
+            // 可提现佣金
+            $commissionCount = bcsub((string)$brokerage_price, (string)$broken_commission, 2);
+            if ($commissionCount < $brokerage) {
+                throw new AutoProfitSharingException('用户的可提现佣金 小于分账金额。');
+            }
+
+            return $user;
+        } catch (Throwable $throwable) {
+            Log::error($throwable->getMessage() . PHP_EOL . json_encode(compact('uid', 'brokerage')));
+        }
+
+        return null;
+    }
+
+    /**
      * 添加分账接收方
      * @param int $user_id
      * @return array
@@ -47,12 +280,8 @@ class CrmebHelper
      */
     public static function addReceiver(int $user_id): array
     {
-        $api = Helper::api();
-
         $openid = self::getUserOpenid($user_id);
-        $response = $api->addReceiver(Utils::packReceiver($openid));
-
-        return $response->toArray();
+        return Helper::addReceiver($openid);
     }
 
     /**
@@ -63,99 +292,7 @@ class CrmebHelper
      */
     public static function removeReceiver(int $user_id): array
     {
-        $api = Helper::api();
-
         $openid = self::getUserOpenid($user_id);
-        $receiver = [
-            'type' => ReceiverTypeEnums::PERSONAL_OPENID,
-            'account' => $openid,
-        ];
-
-        $response = $api->removeReceiver(['receiver' => json_encode($receiver)]);
-
-        return $response->toArray();
-    }
-
-    /**
-     * 请求单次分账（请求需要双向证书）
-     * @param array|Receiver[] $data
-     * @param string $transaction_id
-     * @param string $out_order_no
-     * @return array
-     * @throws HttpException
-     */
-    public static function singleSharing(array $data, string $transaction_id, string $out_order_no): array
-    {
-        $api = Helper::api();
-        $params = Utils::builderProfitSharing($data, $transaction_id, $out_order_no);
-
-        //var_dump('请求单次分账（请求需要双向证书）【请求入参】', $params);
-        $response = $api->single($params);
-        //var_dump('请求单次分账（请求需要双向证书）【响应】', $response);
-
-        return $response->toArray();
-    }
-
-    /**
-     * 请求多次分账（请求需要双向证书）
-     * @param array|Receiver[] $data
-     * @param string $transaction_id
-     * @param string $out_order_no
-     * @return array
-     * @throws HttpException
-     */
-    public static function multiSharing(array $data, string $transaction_id, string $out_order_no): array
-    {
-        $api = Helper::api();
-        $params = Utils::builderProfitSharing($data, $transaction_id, $out_order_no);
-
-        $response = $api->multi($params);
-
-        return $response->toArray();
-    }
-
-    /**
-     * 完结分账
-     * @param string $transaction_id 微信支付订单号
-     * @param string $out_order_no 商户分账单号【商户系统内部的分账单号，在商户系统内部唯一（单次分账、多次分账、完结分账应使用不同的商户分账单号），同一分账单号多次请求等同一次。只能是数字、大小写字母_-|*@】
-     * @param string $description 分账完结描述
-     * @return array
-     * @throws HttpException
-     */
-    public static function finish(string $transaction_id, string $out_order_no, string $description): array
-    {
-        $api = Helper::api();
-        $response = $api->finish(['transaction_id' => $transaction_id, 'out_order_no' => $out_order_no, 'description' => $description]);
-
-        return $response->toArray();
-    }
-
-    /**
-     * 查询分账结果
-     * @param string $transaction_id 微信支付订单号
-     * @param string $out_order_no 商户分账单号【查询分账结果，输入申请分账时的商户分账单号； 查询分账完结执行的结果，输入发起分账完结时的商户分账单号】
-     * @return array
-     * @throws HttpException
-     */
-    public static function query(string $transaction_id, string $out_order_no): array
-    {
-        $api = Helper::api();
-        $response = $api->query(['transaction_id' => $transaction_id, 'out_order_no' => $out_order_no]);
-
-        return $response->toArray();
-    }
-
-    /**
-     * 查询订单待分账金额
-     * @param string $transaction_id 微信支付订单号
-     * @return array
-     * @throws HttpException
-     */
-    public static function orderAmountQuery(string $transaction_id): array
-    {
-        $api = Helper::api();
-        $response = $api->orderAmountQuery(['transaction_id' => $transaction_id]);
-
-        return $response->toArray();
+        return Helper::removeReceiver($openid);
     }
 }
---
+++ src/Helpers/Helper.php
@@ -0,0 +1,178 @@
+<?php
+
+namespace Ledc\WechatPayProfitSharing\Helpers;
+
+use Ledc\WechatPayProfitSharing\Config;
+use Ledc\WechatPayProfitSharing\Contracts\Receiver;
+use Ledc\WechatPayProfitSharing\Contracts\ReceiverTypeEnums;
+use Ledc\WechatPayProfitSharing\Exceptions\HttpException;
+use Ledc\WechatPayProfitSharing\ProfitService;
+use Ledc\WechatPayProfitSharing\Utils;
+use think\App;
+
+/**
+ * 普通直连分账，助手类
+ */
+class Helper
+{
+    /**
+     * 是否启用微信分账的键名
+     */
+    const WECHAT_PAY_PROFIT_SHARING = 'wechat_pay_profit_sharing';
+
+    /**
+     * 是否需要分账
+     * @return bool
+     */
+    public static function isProfitSharing(): bool
+    {
+        return sys_config(self::WECHAT_PAY_PROFIT_SHARING, false) || getenv(strtoupper(self::WECHAT_PAY_PROFIT_SHARING));
+    }
+
+    /**
+     * 创建实例（微信支付普通直连分账）
+     * - CRMEB单商户 版本号：CRMEB-BZ v5.4.0(20240708)
+     * @return ProfitService
+     */
+    public static function api(): ProfitService
+    {
+        /** @var App $app */
+        $app = app();
+        if ($app->exists(ProfitService::class)) {
+            return $app->make(ProfitService::class);
+        }
+
+        // 是否注入配置
+        if ($app->exists(Config::class)) {
+            $config = $app->make(Config::class);
+        } else {
+            $payment = [
+                'mch_id' => sys_config('pay_weixin_mchid'),
+                'appid' => sys_config('routine_appId') ?: sys_config('wechat_app_appid'),
+                'v2_secret_key' => sys_config('pay_weixin_key'),
+                'secret_key' => sys_config('pay_weixin_key_v3'),
+                'certificate' => substr(public_path(parse_url(sys_config('pay_weixin_client_cert'))['path']), 0, strlen(public_path(parse_url(sys_config('pay_weixin_client_cert'))['path'])) - 1),
+                'private_key' => substr(public_path(parse_url(sys_config('pay_weixin_client_key'))['path']), 0, strlen(public_path(parse_url(sys_config('pay_weixin_client_key'))['path'])) - 1),
+                'serial_no' => sys_config('pay_weixin_serial_no'),
+            ];
+            // 实例化，绑定到容器
+            $config = new Config($payment);
+            $app->instance(Config::class, $config);
+        }
+
+        // 实例化，绑定到容器
+        $service = new ProfitService($config);
+        $app->instance(ProfitService::class, $service);
+
+        return $service;
+    }
+
+    /**
+     * 添加分账接收方
+     * @param string $openid
+     * @return array
+     * @throws HttpException
+     */
+    public static function addReceiver(string $openid): array
+    {
+        $response = Helper::api()->addReceiver(Utils::packReceiver($openid));
+
+        return $response->toArray();
+    }
+
+    /**
+     * 删除分账接收方
+     * @param string $openid
+     * @return array
+     * @throws HttpException
+     */
+    public static function removeReceiver(string $openid): array
+    {
+        $receiver = [
+            'type' => ReceiverTypeEnums::PERSONAL_OPENID,
+            'account' => $openid,
+        ];
+
+        $response = Helper::api()->removeReceiver(['receiver' => json_encode($receiver)]);
+
+        return $response->toArray();
+    }
+
+    /**
+     * 请求单次分账（请求需要双向证书）
+     * @param array|Receiver[] $data
+     * @param string $transaction_id
+     * @param string $out_order_no
+     * @return array
+     * @throws HttpException
+     */
+    public static function singleSharing(array $data, string $transaction_id, string $out_order_no): array
+    {
+        $params = Utils::builderProfitSharing($data, $transaction_id, $out_order_no);
+
+        //var_dump('请求单次分账（请求需要双向证书）【请求入参】', $params);
+        $response = Helper::api()->single($params);
+        //var_dump('请求单次分账（请求需要双向证书）【响应】', $response);
+
+        return $response->toArray();
+    }
+
+    /**
+     * 请求多次分账（请求需要双向证书）
+     * @param array|Receiver[] $data
+     * @param string $transaction_id
+     * @param string $out_order_no
+     * @return array
+     * @throws HttpException
+     */
+    public static function multiSharing(array $data, string $transaction_id, string $out_order_no): array
+    {
+        $params = Utils::builderProfitSharing($data, $transaction_id, $out_order_no);
+
+        $response = Helper::api()->multi($params);
+
+        return $response->toArray();
+    }
+
+    /**
+     * 完结分账
+     * @param string $transaction_id 微信支付订单号
+     * @param string $out_order_no 商户分账单号【商户系统内部的分账单号，在商户系统内部唯一（单次分账、多次分账、完结分账应使用不同的商户分账单号），同一分账单号多次请求等同一次。只能是数字、大小写字母_-|*@】
+     * @param string $description 分账完结描述
+     * @return array
+     * @throws HttpException
+     */
+    public static function finish(string $transaction_id, string $out_order_no, string $description): array
+    {
+        $response = Helper::api()->finish(['transaction_id' => $transaction_id, 'out_order_no' => $out_order_no, 'description' => $description]);
+
+        return $response->toArray();
+    }
+
+    /**
+     * 查询分账结果
+     * @param string $transaction_id 微信支付订单号
+     * @param string $out_order_no 商户分账单号【查询分账结果，输入申请分账时的商户分账单号； 查询分账完结执行的结果，输入发起分账完结时的商户分账单号】
+     * @return array
+     * @throws HttpException
+     */
+    public static function query(string $transaction_id, string $out_order_no): array
+    {
+        $response = Helper::api()->query(['transaction_id' => $transaction_id, 'out_order_no' => $out_order_no]);
+
+        return $response->toArray();
+    }
+
+    /**
+     * 查询订单待分账金额
+     * @param string $transaction_id 微信支付订单号
+     * @return array
+     * @throws HttpException
+     */
+    public static function orderAmountQuery(string $transaction_id): array
+    {
+        $response = Helper::api()->orderAmountQuery(['transaction_id' => $transaction_id]);
+
+        return $response->toArray();
+    }
+}
